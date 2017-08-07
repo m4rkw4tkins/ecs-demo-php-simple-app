4acc7bbb1c0e8eb340ce5c1e7332084feec3c69f
m4rkw4tkins <mark.watkins@youview.com>
2017-08-07 17:42:36 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Hello - J-nak</h1>
+                <h1>Hello - mARK</h1>
                 <h2>New deployment</h2>
                 <p>This application illustrates ECS as part of a pipeline</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
